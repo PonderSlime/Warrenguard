baeaf35259ae7ba395c64fbc803c1d737dbacbf7
PonderSlime <micah.j.edwards1@gmail.com>
2024-07-16 20:28:59 -0600

    Files moved into respective folders

---
+++ project/Prepped Assets/Environment/spikes.gd
@@ -0,0 +1,18 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+	
+func _physics_process(_delta):
+	pass
+
+
+func _on_area_2d_body_entered(body):
+	if body.name == "Player":
+		GlobalVariableLoader.player_health -= 1
+		body.spike()
---
+++ project/Scenes/Test/test_scene_1.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+
+# Add to 'scene_1.gd'.
+
+func _input(event):
+	#GlobalVariableLoader.goto_scene("res://Prepped Assets/Characters/player.tscn")
+	pass
