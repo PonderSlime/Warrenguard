61d806e8452d12a01e392ecdecd0c8f4821ac5c1
PonderSlime <micah.j.edwards1@gmail.com>
2024-07-21 11:47:36 -0600

    Fluorescent fungi spritesheet created

--- project/Prepped Assets/Environment/fungi/florescent_fungi_left.gd
+++ project/Prepped Assets/Environment/fungi/florescent_fungi_left.gd
@@ -13,36 +13,5 @@ func _physics_process(_delta):
 
 
 func _on_area_2d_body_entered(body):
-		if fungi.rotation == 90:
-			if body.name == "Player":
-				print("test")
-				body.fungi_right()
-		elif fungi.rotation == 45:
-			if body.name == "Player":
-				print("test")
-				body.fungi_up_right()
-		elif fungi.rotation == 0:
-			if body.name == "Player":
-				print("test")
-				body.fungi_up()
-		elif fungi.rotation == -45:
-			if body.name == "Player":
-				print("test")
-				body.fungi_up_left()
-		elif fungi.rotation == -90:
-			if body.name == "Player":
-				print("test")
-				body.fungi_left()
-		elif fungi.rotation == -135:
-			if body.name == "Player":
-				print("test")
-				body.fungi_down_left()
-		elif fungi.rotation == -180:
-			if body.name == "Player":
-				print("test")
-				body.fungi_down()
-		elif fungi.rotation == -225:
-			if body.name == "Player":
-				print("test")
-				body.fungi_down_right()
-		
+	if body.name == "Player":
+		body.fungi_left()
