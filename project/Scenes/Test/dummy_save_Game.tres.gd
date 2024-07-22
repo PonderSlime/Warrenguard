9672e592214e027dac90290deae3e8d18c13aa68
PonderSlime <micah.j.edwards1@gmail.com>
2024-07-21 19:46:17 -0600

    Custom background capability added

--- project/Scenes/Test/dummy_save_Game.tres.gd
+++ project/Scenes/Test/dummy_save_Game.tres.gd
@@ -1,6 +1,17 @@
 extends Node2D
-
+@export var time : int = 2
+@export var color_mod : CanvasModulate
+@export var color_sky_mod : CanvasModulate
 func load_game():
 	pass
 func save_game():
 	pass
+
+func _process(delta):
+	if time == 2:
+		color_mod.color = Color(1,1,1)
+		color_sky_mod.color = Color(1,1,1)
+	elif time == 0:
+		color_mod.color = Color(0.25,0.25,0.25)
+		color_sky_mod.color = Color(0.25,0.25,0.25)
+		
