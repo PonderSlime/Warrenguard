c48b37b151b8fae01c0dd69ee4df44ff6ba5801b
PonderSlime <micah.j.edwards1@gmail.com>
2024-07-22 08:59:02 -0600

    Fluorescent fungi files fixed

---
+++ project/Prepped Assets/Environment/fungi/florescent_fungi.gd
@@ -0,0 +1,39 @@
+extends StaticBody2D
+@onready var audio = $AudioStreamPlayer2D
+@onready var state_machine = $AnimationTree.get("parameters/playback")
+var degree : int
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+	
+func _process(delta):
+	degree = round_to_dec(rotation_degrees, 1)
+	print(degree)
+	
+func round_to_dec(num, digit):
+	return round(num * pow(10.0, digit)) / pow(10.0, digit)
+
+func _on_area_2d_body_entered(body):
+	if body.name == "Player":
+		state_machine.travel("boing")
+		audio.stop()
+		audio.play()
+		if degree == 0:
+			body.fungi_up()
+		elif degree == 45 or degree == -315:
+			body.fungi_up_right()
+		elif degree == 90 or degree == -270:
+			body.fungi_right()
+		elif degree == 135 or degree == -225:
+			body.fungi_down_right()
+		elif degree == 180 or degree == -180:
+			body.fungi_down()
+		elif degree == 225 or degree == -135:
+			body.fungi_down_left()
+		elif degree == 270 or degree == -90:
+			body.fungi_left()
+		elif degree == 315 or degree == -45:
+			body.fungi_up_left()
