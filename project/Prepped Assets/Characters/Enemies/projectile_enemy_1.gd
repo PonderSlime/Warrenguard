25294e058f7b32d9a302b679459a81f7a3429d8e
PonderSlime <micah.j.edwards1@gmail.com>
2024-07-23 13:16:11 -0600

    Projectile enemy texture rebuild

--- project/Prepped Assets/Characters/Enemies/projectile_enemy_1.gd
+++ project/Prepped Assets/Characters/Enemies/projectile_enemy_1.gd
@@ -1,5 +1,6 @@
 extends StaticBody2D
-
+@export var head : Sprite2D
+@export var headAnim : AnimationPlayer
 var projectile1_scene = preload("res://Prepped Assets/Characters/Enemies/enemy_projectile_1.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,13 +9,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	look_at(GlobalVariableLoader.player_pos)
+	head.look_at(GlobalVariableLoader.player_pos)
 
 func _on_shoot_timer_timeout():
 	print("timeout!")
 	var b = projectile1_scene.instantiate()
 	get_tree().root.add_child(b)
-	b.start(position)
-	b.transform = global_transform
+	headAnim.play("belch_proj")
+	b.start(head.position)
+	b.transform = head.global_transform
 	$ShootTimer.wait_time = randf_range(4, 10)
 	$ShootTimer.start()
