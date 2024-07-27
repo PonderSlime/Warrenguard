76c86eedaa7604f952ba63ff2845ab69e602f313
PonderSlime <micah.j.edwards1@gmail.com>
2024-07-27 15:38:38 -0600

    Level select screen added

--- project/NextLevelEntrance.gd
+++ project/NextLevelEntrance.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @onready var level_1 = "res://Scenes/Test/test_scene_1.tscn"
 @onready var level_2 = "res://Scenes/Test/test_lava_scene_1.tscn"
+@onready var level_select = "res://Prepped Assets/UI/level_select.tscn"
 @export var level_count : int = 2
 @export var _anim_player : AnimationPlayer
 # Called when the node enters the scene tree for the first time.
@@ -17,13 +18,14 @@ func _on_body_entered(body):
 	if body.name == "PlayerRPG":
 		_anim_player.play("dissolve")
 		await get_tree().create_timer(0.8333).timeout
-		if GlobalVariableLoader.progress_level == 0:
-			await get_tree().create_timer(0.8333).timeout
-			GlobalVariableLoader.goto_scene(level_1)
-			GlobalVariableLoader.switch_scene = true
-			GlobalVariableLoader.active_level = 2
-		elif GlobalVariableLoader.progress_level == 1:
-			GlobalVariableLoader.goto_scene(level_2)
-			GlobalVariableLoader.switch_scene = true
-			GlobalVariableLoader.active_level = 3
+		GlobalVariableLoader.goto_scene(level_select)
+		#if GlobalVariableLoader.progress_level == 0:
+			#await get_tree().create_timer(0.8333).timeout
+			#GlobalVariableLoader.goto_scene(level_1)
+			#GlobalVariableLoader.switch_scene = true
+			#GlobalVariableLoader.active_level = 2
+		#elif GlobalVariableLoader.progress_level == 1:
+			#GlobalVariableLoader.goto_scene(level_2)
+			#GlobalVariableLoader.switch_scene = true
+			#GlobalVariableLoader.active_level = 3
 				
---
+++ project/Prepped Assets/UI/level_select.gd
@@ -0,0 +1,28 @@
+extends Node2D
+func load_game():
+	pass
+func save_game():
+	pass
+@export var scene_type : int = 2
+@export var scene_1 = "res://Scenes/Test/test_scene_1.tscn"
+@export var scene_2 = "res://Scenes/Test/test_lava_scene_1.tscn"
+@onready var _anim_player = $SceneTransition/AnimationPlayer
+func _ready():
+	GlobalVariableLoader.active_level = scene_type
+	
+func _process(delta):
+	if !$TileMap/Sprite2D/AudioStreamPlayer2D.is_playing():
+		$TileMap/Sprite2D/AudioStreamPlayer2D.play()
+
+
+func _on_level_1_start_pressed():
+	_anim_player.play("dissolve")
+	await get_tree().create_timer(0.8333).timeout
+	GlobalVariableLoader.goto_scene(scene_1)
+
+
+func _on_level_2_start_pressed():	
+	if GlobalVariableLoader.progress_level >= 1:
+		_anim_player.play("dissolve")
+		await get_tree().create_timer(0.8333).timeout
+		GlobalVariableLoader.goto_scene(scene_2)
